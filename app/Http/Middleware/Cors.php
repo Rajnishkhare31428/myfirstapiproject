3ef19dacb4d2aa27b30fbe3e986cc8de8f25199e
RAJNEESH KHARE <40913547+Rajnishkhare31428@users.noreply.github.com>
2020-10-29 23:55:30 +0530

    modified origins to be allowed by cors policy

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -18,7 +18,7 @@ class Cors
     public function handle($request, Closure $next)
     {
       return $next($request)
-        ->header('Access-Control-Allow-Origin', 'http://iamrajnish.atspace.cc')
+        ->header('Access-Control-Allow-Origin', 'http://iamrajnish.atspace.cc/create.html, http://iamrajnish.atspace.cc/update.html, http://iamrajnish.atspace.cc/view.html, http://iamrajnish.atspace.cc/delete.html, http://iamrajnish.atspace.cc')
         ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
         ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization, accept');  
     }
